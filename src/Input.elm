ffb04ccb4629a962157873eba72321dc1b92d5fa
Graeme Coupar <grambo@grambo.me.uk>
2019-07-13 13:04:55 +0100

    Add Input w/ Parser
    This will allow us to parse the text input as requested in the
    exercise description.

---
+++ src/Input.elm
@@ -0,0 +1,120 @@
+module Input exposing (Input, RobotInput, RobotInputs, parse, robotInputParser)
+
+import Maybe.Extra
+import Parser exposing ((|.), (|=), Parser, Step(..), end, int, loop, map, oneOf, spaces, succeed, symbol, variable)
+import Robot exposing (Coord, Instruction(..), Orientation(..), RobotPosition(..), RobotState(..))
+import Set
+
+
+type alias RobotInput =
+    { startingState : RobotState, instructions : List Instruction }
+
+
+type alias RobotInputs =
+    List RobotInput
+
+
+type alias Input =
+    { maxCoords : Coord, robots : List RobotInput }
+
+
+parse : Parser Input
+parse =
+    succeed Input
+        |= coordParser
+        |. spaces
+        |= loop [] robotsParserHelp
+        --|= (robotInputParser |> Parser.map List.singleton)
+        |. end
+
+
+coordParser : Parser Coord
+coordParser =
+    succeed (\x y -> ( x, y ))
+        |= int
+        |. spaces
+        |= int
+
+
+orientationParser : Parser Orientation
+orientationParser =
+    oneOf
+        [ "N" |> symbol |> map (\_ -> North)
+        , "S" |> symbol |> map (\_ -> South)
+        , "E" |> symbol |> map (\_ -> East)
+        , "W" |> symbol |> map (\_ -> West)
+        ]
+
+
+robotsParserHelp : RobotInputs -> Parser (Step RobotInputs RobotInputs)
+robotsParserHelp revInputs =
+    oneOf
+        [ succeed (\stmt -> Loop (stmt :: revInputs))
+            |= robotInputParser
+
+        --|. spaces
+        , succeed
+            ()
+            |> map (\_ -> Done (List.reverse revInputs))
+        ]
+
+
+robotInputParser : Parser RobotInput
+robotInputParser =
+    succeed
+        (\coords orientation instructions ->
+            RobotInput (KnownPosition (RobotPosition coords orientation)) instructions
+        )
+        |= coordParser
+        |. spaces
+        |= orientationParser
+        |. spaces
+        |= instructionsParser
+        |. spaces
+
+
+instructionsParser : Parser (List Instruction)
+instructionsParser =
+    let
+        isInstructionChar c =
+            c == 'F' || c == 'L' || c == 'R'
+    in
+    variable
+        { start = isInstructionChar
+        , inner = isInstructionChar
+        , reserved = Set.empty
+        }
+        |> Parser.andThen
+            (\str ->
+                str
+                    |> String.toList
+                    |> List.map charToInstruction
+                    |> Maybe.Extra.combine
+                    |> maybeToParser
+            )
+
+
+charToInstruction : Char -> Maybe Instruction
+charToInstruction c =
+    case c of
+        'R' ->
+            Just TurnRight
+
+        'L' ->
+            Just TurnLeft
+
+        'F' ->
+            Just MoveForward
+
+        _ ->
+            Nothing
+
+
+maybeToParser : Maybe a -> Parser a
+maybeToParser m =
+    case m of
+        Just x ->
+            succeed x
+
+        Nothing ->
+            Parser.problem "Unexpected instruction character"
---
+++ tests/InputTests.elm
@@ -0,0 +1,59 @@
+module ParserTests exposing (suite)
+
+import Expect exposing (Expectation)
+import Fuzz exposing (Fuzzer, int, list, string)
+import Input exposing (..)
+import Parser
+import Robot exposing (..)
+import Test exposing (..)
+
+
+suite : Test
+suite =
+    describe "Parser.parse"
+        [ test "can parse a single robots input" <|
+            \_ ->
+                let
+                    result =
+                        Parser.run Input.parse "5 3\n1 1 E\nRFRL"
+                in
+                Expect.equal result
+                    (Ok
+                        (Input ( 5, 3 )
+                            [ { startingState = KnownPosition (RobotPosition ( 1, 1 ) East)
+                              , instructions = [ TurnRight, MoveForward, TurnRight, TurnLeft ]
+                              }
+                            ]
+                        )
+                    )
+        , test "can parse multiple robots input" <|
+            \_ ->
+                let
+                    result =
+                        Parser.run Input.parse "5 3\n1 1 E\nRFRL\n2 2 W\nR"
+                in
+                Expect.equal result
+                    (Ok
+                        (Input ( 5, 3 )
+                            [ { startingState = KnownPosition (RobotPosition ( 1, 1 ) East)
+                              , instructions = [ TurnRight, MoveForward, TurnRight, TurnLeft ]
+                              }
+                            , { startingState = KnownPosition (RobotPosition ( 2, 2 ) West)
+                              , instructions = [ TurnRight ]
+                              }
+                            ]
+                        )
+                    )
+        , test "robot input parser" <|
+            \_ ->
+                let
+                    result =
+                        Parser.run Input.robotInputParser "1 1 E\nRFRL"
+                in
+                Expect.equal result
+                    (Ok
+                        { startingState = KnownPosition (RobotPosition ( 1, 1 ) East)
+                        , instructions = [ TurnRight, MoveForward, TurnRight, TurnLeft ]
+                        }
+                    )
+        ]
